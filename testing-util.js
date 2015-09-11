72f1dfca7c9a174ac23dc4d6cabca4f62abcc46f
HD-LisaH <lisah@hairdirect.com>
2015-09-11 09:17:49 -0400

    remove js

--- testing-util.js
+++
@@ -1,8 +0,0 @@
-$('<style type="text/css"> .round-button-active { background-color: #5088ba; } </style>').appendTo(document.head);
-
-$(document).on('click','label.card', function (event) {
-		  var $this = $(this);
-		  $(this).children(":first").toggleClass("round-button-active").siblings().removeClass('round-button-active');;
-		  evt.preventDefault()
-  //alert('Clicked');
-});
